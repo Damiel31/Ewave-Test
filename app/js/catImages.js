b13275666f66eded92a76844d4fd461850091a25
Damiel31 <damielkun@gmail.com>
2022-01-22 22:13:23 +0800

    changes kitty section

--- app/js/catImages.js
+++ app/js/catImages.js
@@ -1,11 +1,32 @@
-$("document").ready(function () {
-    $.getJSON("https://api.thecatapi.com/v1/images/search", (data) => {
-        $('.catImage').attr('src', data[0].url);
-    })
+$("document").ready( () => {
+    
+    let counter = 0;
+    $.ajax({
+        type: "GET",
+        url: "https://api.thecatapi.com/v1/images/search?order=0&limit=3",
+        dataType: "JSON",
+        success: (data) => {
+            $('.catImage').attr('src', data[0].url);
+            $('.img2').attr('src', data[1].url);
+            $('.img3').attr('src', data[2].url);
+        }
+    });
 
     $('.load_more_catimage').click(() => {
-        $.getJSON("https://api.thecatapi.com/v1/images/search", (data) => {
-            $('<img />').attr('src', data[0].url).appendTo($('.loadCats__catImages'));
-    })
+        $('<div />').addClass('addedImages' + counter).appendTo($('.loadCats__catImages'));
+        let name = '.addedImages' + counter;
+        $.ajax({
+            type: "GET",
+            url: "https://api.thecatapi.com/v1/images/search?order=0&limit=3",
+            dataType: "JSON",
+            success: (data) => {
+
+                $(name).append('<img src="' + data[0].url + '" />');
+                $(name).append('<img src="' + data[1].url + '" />');
+                $(name).append('<img src="' + data[2].url + '" />');
+            }
+        });
+
+        counter += 1;
     })
 });
